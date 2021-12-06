873fd4e9e4310b55352f71c6c8148845b3582f03
yongseongjeon <yongseong.jeon95@gmail.com>
2021-12-06 14:36:25 +0900

    feat: canAdvance, setRacingCount

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 class Car {
-  // 자동차를 생성하는 기능
-  constructor(names) {
-    const namesArr = names.split(",");
+  constructor() {
+    this.names = [];
+    this.locations = [];
+    this.racingCount = 0;
+
+    this.init();
+  }
+
+  init() {
+    this.carNamesSubmitBtn = document.getElementById("car-names-submit");
+    this.racingCountSubmitBtn = document.getElementById("racing-count-submit");
+    this.carNamesInputEl = document.getElementById("car-names-input");
+    this.racingCountInputEl = document.getElementById("racing-count-input");
+    this.handleInput();
+  }
+
+  // 자동차를 입력받아 생성하는 기능
+  createCar() {
+    const namesArr = this.carNamesInputEl.value.split(",");
     const namesNum = namesArr.length;
 
     this.names = namesArr;
@@ -14,20 +30,38 @@ class Car {
   }
 
   // 자동차가 전진하는 기능
-  fowardCar(carName) {
+  advanceCar(carName) {
     const carIdx = this.getCarIdx(carName);
     this.locations[carIdx] += 1;
   }
+
+  handleInput() {
+    this.carNamesSubmitBtn.addEventListener("click", () => {
+      this.createCar();
+      console.log(this);
+    });
+
+    this.racingCountSubmitBtn.addEventListener("click", () => {
+      this.setRacingCount();
+      console.log(this);
+    });
+  }
+
+  // 전진하는 조건을 판별하는 기능
+  canAdvance(number) {
+    const NUM = 4;
+    return number >= NUM;
+  }
+
+  // 몇 번의 이동을 할 것인지 입력 받는 기능
+  setRacingCount() {
+    this.racingCount = this.racingCountInputEl.value;
+  }
 }
 
-const car = new Car("a,b,c,d");
+const car = new Car();
 
-// 자동차에 이름을 부여하는 기능
-// 전진하는 조건을 판별하는 기능
 // 우승자를 고르는 기능
-// 자동차 이름을 입력 받는 기능
-// 몇 번의 이동을 할 것인지 입력 받는 기능
 // 잘못된 입력 값인 경우 alert를 보여주는 기능
 // 자동차의 위치를 출력하는 기능
 // 우승자를 출력하는 기능
-
