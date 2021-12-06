e43a80b2825de3a25e48b4946e663e00fc89a8c7
pmhxhsj <minho7952@naver.com>
2021-12-06 16:44:43 +0900

    feat: 새로고침 이벤트 모듈로 분리 - 가독성을 위해 모듈로 분리

---
+++ src/formEvent.js
@@ -0,0 +1,11 @@
+function formEvent() {
+  const allForm = document.getElementsByTagName('form');
+
+  for (const $form of allForm) {
+    $form.addEventListener('submit', (e) => {
+      e.preventDefault();
+    });
+  }
+}
+
+export { formEvent };
