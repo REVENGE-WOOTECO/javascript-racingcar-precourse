d75c778a824481d32f470144c80b8f09304c708a
yongseongjeon <yongseong.jeon95@gmail.com>
2021-12-06 13:50:03 +0900

    feat: advance car

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 class Car {
   // 자동차를 생성하는 기능
   constructor(names) {
-    const namesArr = names.split(',');
+    const namesArr = names.split(",");
     const namesNum = namesArr.length;
 
     this.names = namesArr;
     this.locations = Array(namesNum).fill(0);
   }
+
+  // 차 이름으로 인덱스를 찾는 기능
+  getCarIdx(carName) {
+    return this.names.indexOf(carName);
+  }
+
+  // 자동차가 전진하는 기능
+  fowardCar(carName) {
+    const carIdx = this.getCarIdx(carName);
+    this.locations[carIdx] += 1;
+  }
 }
 
-const car = new Car('a,b,c,d');
-console.log('car :', car);
+const car = new Car("a,b,c,d");
 
-// 자동차가 전진하는 기능 (차 이름) => 그 차 locations++
-// 자동차가 멈추는 기능
 // 자동차에 이름을 부여하는 기능
 // 전진하는 조건을 판별하는 기능
 // 우승자를 고르는 기능
@@ -22,3 +30,4 @@ console.log('car :', car);
 // 잘못된 입력 값인 경우 alert를 보여주는 기능
 // 자동차의 위치를 출력하는 기능
 // 우승자를 출력하는 기능
+
