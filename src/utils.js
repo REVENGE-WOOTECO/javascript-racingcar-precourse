b1a6cd527207c96fa8b56e2c5d17810e60846de0
veggie-garden <devcseo@gmail.com>
2021-12-17 22:06:15 +0900

    chore: make two appendChild to one

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import { RESULT_TEXT } from "./constants.js";
 
 export const generateRandomNumber = () => {
+  // eslint-disable-next-line no-undef
   return MissionUtils.Random.pickNumberInRange(0, 9);
 };
 
@@ -10,6 +11,7 @@ export const printResultOneTurn = (cars) => {
   let resultCarText = "";
 
   for (let i = 0; i < cars.length; i += 1) {
+    // eslint-disable-next-line prettier/prettier
     resultCarText += `${cars[i].carName}: ${"-".repeat(cars[i].forwardCount)}\n`;
   }
 
@@ -42,6 +44,5 @@ export const printWinner = (winnerList) => {
   resultSpan.innerText = RESULT_TEXT;
   winnerSpan.innerText = winnerList.join(", ");
 
-  app.appendChild(resultSpan);
-  app.appendChild(winnerSpan);
+  app.appendChild(resultSpan, winnerSpan);
 };
